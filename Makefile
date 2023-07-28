ac8cf9a3c34d0dd67540a36ea6b733198fe59c89
= <=>
2023-07-28 08:26:35 +0200

    Minor changes

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ make:
 
 clean:
 	rm run
-	g++ -o run main.cpp headers/*.h
+	
--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 int main()
 {
 	place_holder::hello_world();
-
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << i << std::endl;
+	}
 	return 0;
 }
